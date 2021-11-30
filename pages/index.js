833f7a22c7984f9c52a36e45d3615a510659382e
Subham kumar <shubhamkumar7880@gmail.com>
2021-11-30 18:02:54 +0530

    ready for deployment

---
+++ pages/index.js
@@ -0,0 +1,73 @@
+import { Fragment } from "react";
+import Head from "next/head";
+import { MongoClient } from "mongodb";
+import MeetupList from "../components/meetups/MeetupList";
+
+// const DUMMY_MEETUPS = [
+//   {
+//     id: "m1",
+//     title: "A first meetup",
+//     image:
+//       "https://images.unsplash.com/photo-1535827841776-24afc1e255ac?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80",
+//     address: "Some address x, 12345 Some City",
+//     description: "This is a first meetup!",
+//   },
+//   {
+//     id: "m2",
+//     title: "A second meetup",
+//     image:
+//       "https://images.unsplash.com/photo-1566073771259-6a8506099945?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+//     address: "Some address y, 12345 Some City",
+//     description: "This is a second meetup!",
+//   },
+// ];
+
+function HomePage(props) {
+  return (
+    <Fragment>
+      <Head>
+        <title>React Meetups</title>
+        <meta
+          name="description"
+          content="Browse a huge list of highly active React Meetups!"
+        />
+      </Head>
+      <MeetupList meetups={props.meetups} />;
+    </Fragment>
+  );
+}
+
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//   };
+// }
+export async function getStaticProps() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://khiladi786:khiladi786@cluster0.6xgx6.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find().toArray();
+  client.close();
+  return {
+    props: {
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
+    },
+    revalidate: 3600,
+  };
+}
+
+export default HomePage;
